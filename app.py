bbd1a5ba7633edbd63f38c6e4aff42a86f74c15a
umeshgandla <139842606+umeshgandla@users.noreply.github.com>
2024-11-04 12:15:33 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# application.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World from AWS Elastic Beanstalk!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
