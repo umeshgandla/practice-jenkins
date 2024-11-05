cca9f754485d360d6f1c7fef5589864a73aa3200
umeshgandla <139842606+umeshgandla@users.noreply.github.com>
2024-11-05 11:03:55 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,5 +9,5 @@ def client():
 def test_hello_world(client):
     """Test the hello world route."""
     response = client.get('/')
-    assert response.data == b'Hello, mama'
+    assert response.data == b'Hello, mamaa'
     assert response.status_code == 200
